e397e436b2cc022369eddb403e51c0cdb828f887
Adam Price <komidore64@gmail.com>
2012-11-29 22:28:30 -0500

    new resume, removed links, and some domestic cleaning
    - reformated resume page (still sucks, but will have to do for now)
    - removed crappy links page
    - reorganized css styling
    - small changes to main index

--- public/content/20_resume.php
+++ public/content/20_resume.php
@@ -1,27 +1,28 @@
 <div style="width: 60%; padding: 25px;">
-    <!-- <p>OBJECTIVE: Further my knowledge and skills whilst developing my career as a software engineer.</p> -->
-    <div style="border-style: dotted; padding: 5px; margin: 5px 0px;">
+    <div class="resume_section">
         <p>SUMMARY</p>
         <p>
-        &#9679; Experienced in programming with proper design principles<br />
-        &#9679; Experience with computer hardware and PC building<br />
-        &#9679; Exceptional academic record and work ethic both at Elon University and in High School<br />
-        &#9679; Achieved Eagle Scout Rank (2008) with the Boy Scouts of America
+            <ul>
+                <li>Experienced in programming with proper design principles</li>
+                <li>Experience with computer hardware and PC building</li>
+                <li>Exceptional academic record and work ethic both at Elon University and in High School</li>
+                <li>Achieved Eagle Scout Rank (2008) with the Boy Scouts of America</li>
+            </ul>
         </p>
     </div>
-    <div style="border-style: dotted; padding: 5px; margin: 5px 0px;">
+    <div class="resume_section">
         <p>EDUCATION</p>
         <p>Elon University, Elon, NC, 2012<br />Degrees: Computer Science &amp; Computer Information Systems</p>
         <p>Advanced Studies Diploma, Glenvar High School, Salem, VA 2008</p>
     </div>
-    <div style="border-style: dotted; padding: 5px; margin: 5px 0px;">
+    <div class="resume_section">
         <p>TECHNICAL QUALIFICATIONS</p>
         <table>
-            <tr>
+            <tr class="resume_bottom_border">
                 <td style="width: 250px;">Programming/ Languages: </td>
                 <td>Java, Ruby, Ruby on Rails, Python, Bash, JavaScript, PHP, Java Servlets &amp; JSPs, C/C++, HTML[5], CSS, Haml, SQL</td>
             </tr>
-            <tr>
+            <tr class="resume_bottom_border">
                 <td style="width: 250px;">Software/ APIs:</td>
                 <td>Vim, Git, GitHub, RubyMine, Eclipse, Oracle VirtualBox, KVM, Propellerhead Reason</td>
             </tr>
@@ -31,7 +32,7 @@
             </tr>
         </table>
     </div>
-    <div style="border-style: dotted; padding: 5px; margin: 5px 0px;">
+    <div class="resume_section">
         <p>WORK HISTORY</p>
         <p>Red Hat Inc., Raleigh, NC<br />Software Engineering Intern (summer 2011)</p>
         <p>New Breed Logistics, High Point, NC<br />Software Development Intern (summer 2010)</p>
